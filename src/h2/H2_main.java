be46b615e53cbccc7629c37665b133488a7c1bd5
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-02 17:22:39 +0100

    h2 bearbeitet

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,25 @@
 package h2;
 
 public class H2_main {
+    public static void main(String[] args) {
+        int jahr = 2346;
+        boolean schalt = false;
+        if (jahr % 4 == 0) {
+            if (jahr % 100 == 0) {
+                if (jahr % 400 == 0) {
+                    schalt = true;
+                }
+                else {
+                    schalt = false;
+                }
+            }
+            else  {
+                schalt = true;
+            }
+        }
+        else{
+            schalt = false;
+        }
+        System.out.println(schalt);
+    }
 }
