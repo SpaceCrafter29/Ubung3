7659fe1631b679b343998f606ebd75abc489476d
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-02 18:13:44 +0100

    h3 bearbeitet

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,4 +1,23 @@
 package h3;
 
 public class H3_main {
+    public static void main(String[] args) {
+        int max = 8000;
+        int fix = 7832;
+        int wartend = 450;
+        boolean istVoll;
+        int freiePlatze = max - fix;
+        if (freiePlatze == 0){
+            istVoll = true;
+        }
+        else {
+            istVoll = false;
+            wartend = wartend - freiePlatze;
+            fix = max;
+            istVoll = true;
+        }
+        System.out.println("Belegte Plätze: " + fix);
+        System.out.println("Personen auf der Warteliste: " + wartend);
+        System.out.println("Stadion Voll? " + istVoll);
+    }
 }
