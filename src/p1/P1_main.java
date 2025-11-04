c70b2f0f5e00e293b8f9f8d53a7f29c4f09a6304
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-04 16:11:52 +0100

    p1 bearbeitet

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,4 +1,19 @@
 package p1;
 
 public class P1_main {
+    public static void main(String[] args) {
+        byte wuerfel = 1;
+        if (wuerfel == 2) {
+            System.out.println("Leider Verloren");
+        }
+        if (wuerfel == 4) {
+            System.out.println("Gewonnen!");
+        }
+        if (wuerfel == 5) {
+            System.out.println("Unentschieden");
+        }
+        if (wuerfel == 1) {
+            System.out.println("Gewonnen!");
+        }
+    }
 }
