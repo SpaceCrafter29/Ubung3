2e7c9a56954c711d59abd0d27acd4ca8732ade9b
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-07 18:18:06 +0100

    wartend kann nicht mehr negativ sein

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -6,15 +6,19 @@ public class P2_main {
         int b = 2;
         int c = 3;
         int d = 0;
+        //E1
         if (a == b){
             d += 1;
         }
+        //E2
         if (a == b | a == c){
             d += 1;
         }
+        //E3
         if ((a + b) >= c){
             d += 1;
         }
+        //E4
         if ((a + b + c) >= 100){
             d += 1;
         }
